fedd2fbae31b859970a753a165d2c7b32f2797d7
mitchelmcfarland <mitchymoo1203@gmail>
2025-12-24 15:55:18 -0500

    setting up structs

--- main.c
+++ main.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 
 int main () {
-	printf("Hello World!\n");
+	int status;
+	struct addrinfo hints;
+	struct addrinfo *servinfo;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	
+	if ((status = getaddrinfo(NULL, "3500", &hints, &servinfo)) != 0) {
+		fprintf(stderr, "Gai error: %s\n", gai_strerror(status));
+		exit(1);
+	}
+
+	char ip_address[INET_ADDRSTRLEN];
+
+	struct addrinfo *p;
+
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		
+		struct sockaddr_in *ipv4;
+		
+		ipv4 = (struct sockaddr_in *)p->ai_addr;
+
+		inet_ntop(AF_INET, &(ipv4->sin_addr), ip_address, INET_ADDRSTRLEN);
+
+		printf("%s\n", ip_address);
+	}
+
+	freeaddrinfo(servinfo);
 }
